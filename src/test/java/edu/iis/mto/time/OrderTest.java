07a6c2117c8ef6a35bbc2b9a06abdabc641c2f65
kateryna <katerynatsarova@gmail.com>
2021-04-18 14:08:57 +0300

    added tests for Order

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -4,16 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
+    @Mock
+    private Clock clock;
+
+    private Order order;
+    private Instant startTime;
+    private Instant testTime;
 
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() throws Exception {
+        order = new Order(clock);
+        startTime = Instant.now();
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
     }
 
+    @Test
+    void whenInvalidHours_cancelOrderState() {
+        testTime = startTime.plus(Order.VALID_PERIOD_HOURS + 1, ChronoUnit.HOURS);
+
+        when(clock.instant()).thenReturn(startTime).thenReturn(testTime);
+
+        order.submit();
+
+        assertEquals(order.getOrderState(), Order.State.CANCELLED);
+    }
+
+    @Test
+    void whenInvalidHours_throwException() {
+        testTime = startTime.plus(Order.VALID_PERIOD_HOURS + 1, ChronoUnit.HOURS);
+
+        when(clock.instant()).thenReturn(startTime).thenReturn(testTime);
+
+        order.submit();
+
+        assertThrows(OrderExpiredException.class, () -> order.confirm());
+    }
 }
